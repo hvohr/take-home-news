8a3e66f3b40f3c5f5b7ff4112ab39e9a43fb1c30
Hollis Vohr <hollis.vohr@gmail.com>
2023-09-18 13:04:52 -0600

    feat: add category navbar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,11 +3,12 @@ import { Routes, Route, NavLink } from "react-router-dom";
 import Home from '../../pages/Home'
 import InduvidualArticle from '../../pages/InduvidualArticle'
 import { data } from '../mockData'
-import {getAllArticles} from '../apiCalls'
+import { getAllArticles } from '../apiCalls'
 import { useEffect, useState } from 'react'
 
 function App() {
   const [articles, setArticles] = useState(data.articles)
+  const [ category, setCategory ] = useState('')
 
   // useEffect (() => {
   //   getAllArticles().then(
@@ -15,12 +16,11 @@ function App() {
   //   )
   // }, [])
 
-
   return (
     <div className="App">
       <Routes>
-        <Route path ='/' element={<Home articles={articles}/>} />
-        <Route path ='/:id' element={<InduvidualArticle articles={articles}/>} />
+        <Route path='/' element={<Home setCategory={setCategory} articles={articles} />} />
+        <Route path='/:id' element={<InduvidualArticle articles={articles} />} />
       </Routes>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,16 +2,22 @@ import './pages.css'
 import ArticleCard from '../components/ArticleCard/ArticleCard'
 
 function Home(props) {
-  console.log(props)
-  // const [category, setCategory] = useState("")
   return (
     <section className='home-container'>
       <header>
         <img alt='two standing stick figures shaking hands' className='site-logo' src={require('../components/images/handshake.png')} />
         <h1 className='title'>USAChronicle</h1>
+        <nav>
+          <button onClick={() => setCategory('business')}>Business</button>
+          <button onClick={() => setCategory('health')}>Health</button>
+          <button onClick={() => setCategory('technology')}>Technology</button>
+          <button onClick={() => setCategory('science')}>Science</button>
+          <button onClick={() => setCategory('entertainment')}>Entertainment</button>
+          <button onClick={() => setCategory('sports')}>Sports</button>
+        </nav>
       </header>
       <section>
-        <ArticleCard articles={props.articles}/>
+        <ArticleCard articles={props.articles} />
       </section>
     </section>
   )

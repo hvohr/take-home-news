176a8319b15873c55b0e9e337fbe7f9325365e75
Hollis Vohr <hollis.vohr@gmail.com>
2023-09-18 10:29:35 -0600

    feat: create mock data

--- src/components/apiCalls.js
+++ src/components/apiCalls.js
@@ -1,5 +1,10 @@
-await function allBusiness() {
-
+async function allBusiness() {
+  const response = await fetch("https://newsapi.org/v2/top-headlines?category=business&apiKey=33a2161beece4720940a9f191b2914bd")
+  if (!response.ok) {
+    throw new Error(response.status)
+  }
+  const data = await response.json()
+  return data
 }
 
 "https://newsapi.org/v2/top-headlines?category=business&apiKey=33a2161beece4720940a9f191b2914bd"

06f77f3429e0b83224df9d6c4b8b17ea3c3d668e
Hollis Vohr <hollis.vohr@gmail.com>
2023-09-18 12:30:23 -0600

    feat: add induvidual article data and error handle for no content

--- src/pages/InduvidualArticle.js
+++ src/pages/InduvidualArticle.js
@@ -1,16 +1,34 @@
 import './pages.css'
-import {useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
 function InduvidualArticle(props) {
   const { id } = useParams();
   const makeID = () => {
     return props.articles.find(art => art.publishedAt === id)
   }
-
-  console.log(makeID())
-  return (
-    <h1>hi</h1>
-  )
+  if (makeID().content === null) {
+    return (
+      <section>
+        <h1 className='single-article-title'>{makeID().title}</h1>
+        <h4>{makeID().author}</h4>
+        <img className='single-article-image' src={makeID().urlToImage}></img>
+        <section>
+          <p>No content available for this article --- visit the <a href={makeID().url}>original article here</a>! We apologize for this inconvienence.</p>
+        </section>
+      </section>
+    )
+  } else {
+    return (
+      <section>
+        <h1 className='single-article-title'>{makeID().title}</h1>
+        <h4>{makeID().author}</h4>
+        <img className='single-article-image' src={makeID().urlToImage}></img>
+        <section>
+          <p>{makeID().content}</p>
+        </section>
+      </section>
+    )
+  }
 }
 
 export default InduvidualArticle
